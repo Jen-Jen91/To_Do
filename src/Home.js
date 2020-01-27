9083606e28076a6dfad6c2c11297778a5d513d7d
Jen Proctor <jen_p_13@hotmail.co.uk>
2020-01-27 14:41:28 +0000

    TextInput to add task to array

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,46 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      tasks: [],
+    };
+  }
+
+  onTaskTyped = task => {
+    console.log('TASK: ', task);
+    this.setState({inputValue: task});
+  };
+
+  onTaskAdded = task => {
+    console.log('TASK: ', task);
+    // eslint-disable-next-line curly
+    if (task.length === 0) return;
+    this.setState(prevState => ({
+      inputValue: '',
+      tasks: [...prevState.tasks, task],
+    }));
+  };
+
   render() {
     return (
       <View>
         <Text>HOME</Text>
+
+        <TextInput
+          value={this.state.inputValue}
+          placeholder="Type a task..."
+          onChangeText={this.onTaskTyped}
+          onSubmitEditing={event => this.onTaskAdded(event.nativeEvent.text)}
+          c
+        />
+
+        {this.state.tasks.map((task, id) => {
+          return <Text key={id}>{task}</Text>;
+        })}
       </View>
     );
   }
